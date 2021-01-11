215b5007c29a07202832f0e76124432a891b9676
khmilevoi <khmilevoi@gmail.com>
2021-01-11 19:25:16 +0200

    some fixes

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,16 @@
 import { Njs } from "./language/njs";
-import { Lexer } from "./lexer/lexer";
 import { IdentifierHandler } from "./lexer/handlers/identifier.handler";
-import { NewLineHandler } from "./lexer/handlers/new-line.handler";
 import { NumberHandler } from "./lexer/handlers/number.handler";
-import { OneLineCommentHandler } from "./lexer/handlers/one-line-comment.handler";
-import { SemicolonHandler } from "./lexer/handlers/semicolon.handler";
 import { ServiceSymbolsHandler } from "./lexer/handlers/service-symbols.handler";
 import { StringHandler } from "./lexer/handlers/string.handler";
+import { Lexer } from "./lexer/lexer";
 import { Logger } from "./logger/logger";
 
 export const njs = new Njs(
   new Logger(),
   new Lexer(
-    new StringHandler('"'),
-    new OneLineCommentHandler("//"),
+    new StringHandler(),
     new IdentifierHandler(),
-    new SemicolonHandler(),
-    new NewLineHandler(),
     new ServiceSymbolsHandler(),
     new NumberHandler()
   )
