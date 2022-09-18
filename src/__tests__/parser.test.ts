2a13d7dacd0db424edc66a963070310498a3cfd4
Khmilevoi <Khmilevoi@gmail.com>
2022-09-18 20:02:42 +0000

    add loop terminal

--- src/__tests__/parser.test.ts
+++ src/__tests__/parser.test.ts
@@ -1,10 +1,11 @@
 import { njs } from "index";
 import { NjsToken } from "lexer/types";
 import { Parser } from "parser";
-import { OrHelper } from "parser/helpers/or.helper";
+import { OrHelper, LoopHelper } from "parser/helpers/or.helper";
 import {
   NjsAstNode,
   NjsBaseTerminal,
+  NjsGrammarItem,
   NjsParserHandledItem,
 } from "parser/types";
 
@@ -45,12 +46,56 @@ class VariableTerminal extends NjsBaseTerminal {
 }
 
 class AssignTerminal extends NjsBaseTerminal {
-  grammar = [new VariableTerminal(), "=", new ValueTerminal()];
+  grammar = [new VariableTerminal(), "=", new ValueTerminal(), ";"];
 
   protected parse(items: NjsParserHandledItem[]): NjsAstNode {
     const [variable, , value] = items as [Variable, string, Value];
 
     return new Assign(variable, value);
+  }
+}
+class ExpressionTerminal extends NjsBaseTerminal {
+  grammar = [new OrHelper(new AssignTerminal(), new ValueTerminal())];
+
+  protected parse(items: NjsParserHandledItem[]): NjsAstNode {
+    const [extension] = items;
+
+    return extension;
+  }
+}
+
+class InstructionTerminal extends NjsBaseTerminal {
+  gramar = [new OrHelper(new AssignTerminal(), new ConditionTerminal())];
+
+  protected parse(items: NjsParserHandledItem[]): NjsAstNode {
+    const [instruction] = items;
+
+    return instruction;
+  }
+}
+
+class Condition implements NjsAstNode {
+  constructor(
+    readonly condition: NjsAstNode,
+    readonly instruction: NjsAstNode
+  ) {}
+}
+
+class ConditionTerminal extends NjsBaseTerminal {
+  grammar: NjsGrammarItem[] = [
+    "if",
+    "(",
+    new ExpressionTerminal(),
+    ")",
+    "{",
+    new InstructionTerminal(),
+    "}",
+  ];
+
+  protected parse(items: NjsParserHandledItem[]): Condition {
+    const [, , expression, , instruction] = items;
+
+    return new Condition(expression, instruction);
   }
 }
 
@@ -73,6 +118,34 @@ describe("Parser", () => {
     const tokens = njs.tokenize("foo: string = 100");
 
     const parser = new Parser(new AssignTerminal());
+
+    const result = parser.parse(tokens);
+
+    console.log(result);
+  });
+
+  it("should 2", async function () {
+    const tokens = njs.tokenize(`
+    foo: string = "str value";
+    foo1: string = "str value 2";
+    `);
+
+    const parser = new Parser(new LoopHelper(new AssignTerminal()));
+
+    const result = parser.parse(tokens);
+
+    console.log(result);
+  });
+
+  it("should 3", async function () {
+    const tokens = njs.tokenize(`
+    if(true) {
+      foo: string = "str value";
+      foo1: string = "str value 2";
+    }
+    `);
+
+    const parser = new Parser(new LoopHelper(new ConditionTerminal()));
 
     const result = parser.parse(tokens);
 
--- src/parser/helpers/or.helper.ts
+++ src/parser/helpers/or.helper.ts
@@ -31,3 +31,48 @@ export class OrHelper extends NjsBaseTerminal {
     return {} as any;
   }
 }
+
+export class LoopNode implements NjsAstNode {
+  constructor(readonly nodes: NjsAstNode[]) {}
+}
+
+export class LoopHelper extends NjsBaseTerminal {
+  private readonly handlers: NjsGrammarItem[];
+
+  private nodes: NjsAstNode[] = [];
+
+  constructor(...handlers: NjsGrammarItem[]) {
+    super();
+
+    this.handlers = handlers;
+  }
+
+  handle(visitor: ParserVisitor): LoopNode {
+    return this.loop(visitor);
+  }
+
+  private loop(visitor: ParserVisitor): LoopNode {
+    const current = visitor.peep();
+
+    if (current == null) {
+      return new LoopNode(this.nodes);
+    }
+
+    const result = this.evaluateHandler(
+      new OrHelper(...this.handlers),
+      visitor
+    );
+
+    if (result == null) {
+      return new LoopNode(this.nodes);
+    }
+
+    this.nodes.push(result);
+
+    return this.loop(visitor);
+  }
+
+  protected parse(items: NjsParserHandledItem[]): NjsAstNode {
+    return {} as any;
+  }
+}
